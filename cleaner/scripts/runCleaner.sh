d28e9cdbb5b33ab7a207ebb0c58fa4d326ded3c5
Michael Dawson <mdawson@devrus.com>
2018-04-29 17:52:38 -0400

    fix typo in run script

--- cleaner/scripts/runCleaner.sh
+++ cleaner/scripts/runCleaner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export INSTALL_ROOT=$HOME
-source $HOME/setups.sh
+source $HOME/setup.sh
 export LOG_FILE=$INSTALL_ROOT/cleaner-log.txt
 touch $LOG_FILE
 node $INSTALL_ROOT/NodePVR/cleaner/lib/index.js >> $LOG_FILE
