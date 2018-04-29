3ed00c3fc2328e3c6551e875fc6cd2d25dbaa640
Michael Dawson <mdawson@devrus.com>
2018-04-29 17:51:12 -0400

    add script for to run from cron

---
+++ cleaner/scripts/runCleaner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export INSTALL_ROOT=$HOME
+source $HOME/setups.sh
+export LOG_FILE=$INSTALL_ROOT/cleaner-log.txt
+touch $LOG_FILE
+node $INSTALL_ROOT/NodePVR/cleaner/lib/index.js >> $LOG_FILE
+
+
