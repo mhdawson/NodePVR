221fc4ed98a2c1930ebfe2ee0ea11c4f829e1556
Michael Dawson <mdawson@devrus.com>
2018-04-30 08:53:35 -0400

    switch to constant frame rate
    switch to constant frame rate to address audio sync issues

--- cleaner/scripts/doCompress.sh
+++ cleaner/scripts/doCompress.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-HandBrakeCLI -i "$1" -c 1 -o "$2" -f mp4 --loose-anamorphic --crop 0:0:0:0  -e x264 -q 24 -a 1 -E faac -6 dpl2 -R 48 -B 160 -D 0.0 -x ref=2:bframes=2:subq=6:mixed-refs=0:weightb=0:8x8dct=0:trellis=0:rc-lookahead=10:threads=6 -v 1
+# HandBrakeCLI -i "$1" -c 1 -o "$2" -f mp4 -t 1 --loose-anamorphic --crop 0:0:0:0  -e x264 -q 24 -a 1 -E faac -6 dpl2 -R 48 -B 160 -D 0.0 -x ref=2:bframes=2:subq=6:mixed-refs=0:weightb=0:8x8dct=0:trellis=0:rc-lookahead=10:threads=6 -v 1
+HandBrakeCLI -i "$1" -c 1 -o "$2" -f mp4 -t 1 --loose-anamorphic --crop 0:0:0:0  --cfr -e x264 -q 24 -a 1 -E faac -r 29.97 -6 dpl2 -R 48 -B 160 -D 0.0 -x ref=2:bframes=2:subq=6:mixed-refs=0:weightb=0:8x8dct=0:trellis=0:rc-lookahead=0:threads=4 -v 1
