ddf478480bae4305b054bfcbf9c89bbecd278379
Michael Dawson <mdawson@devrus.com>
2019-02-18 23:26:01 -0500

    src: add licence test

--- scheduler/lib/index.js
+++ scheduler/lib/index.js
@@ -1,3 +1,6 @@
+// Copyright the project authors as listed in the AUTHORS file.
+// All rights reserved. Use of this source code is governed by the
+// license that can be found in the LICENSE file.
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
